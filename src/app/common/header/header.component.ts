32720ee26946952634d592a6d5a451546afb104b
Roop Padala <roop.padala@macys.com>
2018-07-27 13:32:27 -0700

    Header component

---
+++ src/app/common/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector : 'booking-header',
+    templateUrl : './header.component.html',
+    styleUrls: ['./header.componet.css']
+})
+
+export class HeaderComponent {
+
+}
